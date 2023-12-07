e39425d0d8b8416994cc087f8b184786e7665f0d
traitboundnotsatisfied <144365472+traitboundnotsatisfied@users.noreply.github.com>
2023-12-06 23:33:46 -0600

    getting there

--- dec7/src/main.zig
+++ dec7/src/main.zig
@@ -66,7 +66,7 @@ fn process(filename: []const u8, comptime n: comptime_int) !u64 {
     var n_ranked: usize = 0;
     for (lines) |line| {
         var position = 0;
-        while (beats(line, ranked[position]) and (position <= n_ranked)) {
+        while (beats(line.hand, ranked[position].hand) and (position <= n_ranked)) {
             position += 1;
         }
         for (position..n_ranked) |i| {
@@ -80,4 +80,70 @@ fn process(filename: []const u8, comptime n: comptime_int) !u64 {
         sum += ranked[i].bid * i;
     }
     return sum;
+}
+
+fn beats(a: [5]u8, b: [5]u8) bool {
+    const a_class = assignClass(a);
+    const b_class = assignClass(b);
+    if (a_class > b_class) {
+        return true;
+    }
+    if (a_class < b_class) {
+        return false;
+    }
+    return beatsGivenSameClass(a, b);
+}
+
+fn assignClass(a: [5]u8) u8 {
+    if (n_of_a_kind(a, 5)) return 7;
+    if (n_of_a_kind(a, 4)) return 6;
+    if (full_house(a)) return 5;
+    if (n_of_a_kind(a, 3)) return 4;
+    if (n_pair(a, 2)) return 3;
+    if (n_pair(a, 1)) return 2;
+    return 1;
+}
+
+fn n_of_a_kind(a: [5]u8, n: u8) bool {
+    var n_same: usize = 0;
+    for (0..5) |card_i| {
+        const card = a[card_i];
+        var matches = false;
+        for (0..card_i) |other_card_i| {
+            const other_card = a[other_card_i];
+            if (card == other_card) {
+                matches = true;
+                break;
+            }
+        }
+        if (matches) {
+            n_same += 1;
+        }
+    }
+    return n_same == n;
+}
+
+fn full_house(a: [5]u8) bool {
+    var n_l1: usize = 0;
+    var n_l2: usize = 0;
+    const l1 = a[0];
+    var l2 = 0;
+    for (a) |card| {
+        if (card != l1) {
+            l2 = card;
+            break;
+        }
+    }
+    if (l2 == 0) return false;
+    for (a) |card| {
+        _ = card;
+        if (a == l1) n_l1 += 1;
+        if (a == l2) n_l2 += 1;
+    }
+    return ((n_l1 == 2) and (n_l2 == 3)) or ((n_l1 == 3) and (n_l2 == 2));
+}
+
+fn n_pair(a: [5]u8, n: u8) bool {
+    _ = n;
+    _ = a;
 }
