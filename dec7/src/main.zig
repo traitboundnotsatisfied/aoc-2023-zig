6cbe67f4f576d1394c201f8128cbafc92ebe8a35
traitboundnotsatisfied <144365472+traitboundnotsatisfied@users.noreply.github.com>
2023-12-06 22:59:28 -0600

    alright

---
+++ dec7/src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn main() !void {
+    try test_process("test.txt", 42);
+    const real_sum = try process("input.txt");
+    std.debug.print("The answer for part 1 is {d}.\n", .{real_sum});
+}
+
+fn test_process(filename: []const u8, answer: u64) !void {
+    const test_sum = try process(filename);
+    if (test_sum == answer) {
+        std.debug.print("TEST PASSED\n", .{});
+    } else {
+        std.debug.print("TEST FAILED, got {d} not {d}.\n", .{ test_sum, answer });
+        return error.TestFailed;
+    }
+}
+
+fn process(filename: []const u8) !u64 {
+    _ = filename;
+    return 42;
+}
