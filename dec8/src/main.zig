d81c2f62a1edb973e09a30650e5bffa73fab9bd0
traitboundnotsatisfied <144365472+traitboundnotsatisfied@users.noreply.github.com>
2023-12-07 23:22:38 -0600

    at least it dont crash yk

---
+++ dec8/src/main.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+
+pub fn main() !void {
+    try test_process("test.txt", 6);
+    const real_sum = try process("input.txt");
+    std.debug.print("The answer for part 2 is {d}.\n", .{real_sum});
+}
+
+fn test_process(filename: []const u8, answer: u64) !void {
+    const test_sum = try process(filename);
+    if (test_sum == answer) {
+        std.debug.print("TEST PASSED\n", .{});
+    } else {
+        std.debug.print("TEST FAILED, got {d} not {d}.\n", .{ test_sum, answer });
+        return error.TestFailed;
+    }
+}
+
+const Ident = [3]u8;
+
+fn process(filename: []const u8) !u64 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) @panic("MEMORY LEAK DETECTED!");
+    }
+    var transition = std.AutoHashMap(Ident, struct { Ident, Ident }).init(alloc);
+    defer transition.deinit();
+    var directions = std.ArrayList(u8).init(alloc);
+    defer directions.deinit();
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+    var reader = file.reader();
+    var ch: u8 = undefined;
+    var reading_directions: bool = true;
+    var nothing_so_far = true;
+    var transition_line = [3]u8{ undefined, undefined, undefined };
+    var transition_line_i: u8 = 0;
+    while (true) {
+        ch = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        if (ch == '\n') {
+            if (nothing_so_far) reading_directions = false;
+            nothing_so_far = true;
+            transition_line_i = 0;
+            if (!reading_directions) {
+                var spliterator = std.mem.split(u8, &transition_line, ",");
+                var i: u8 = 0;
+                var src: Ident = undefined;
+                var dst_l: Ident = undefined;
+                var dst_r: Ident = undefined;
+                while (spliterator.next()) |part| {
+                    var target = switch (i) {
+                        0 => &src,
+                        1 => &dst_l,
+                        2 => &dst_r,
+                        else => @panic("invalid value"),
+                    };
+                    for (0..3) |cpy_i| {
+                        target[cpy_i] = part[cpy_i];
+                    }
+                    i += 1;
+                }
+                try transition.put(src, .{ dst_l, dst_r });
+            }
+            continue;
+        } else {
+            nothing_so_far = false;
+        }
+        if (reading_directions) {
+            try directions.append(ch);
+        } else {
+            transition_line[transition_line_i] = ch;
+        }
+    }
+    return 42;
+}
