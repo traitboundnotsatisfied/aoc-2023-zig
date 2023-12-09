f35f4468da1c4292a3ab1ea84501de13b9e842b5
traitboundnotsatisfied <144365472+traitboundnotsatisfied@users.noreply.github.com>
2023-12-09 00:14:14 -0600

    finally cpomiles

--- dec9/src/main.zig
+++ dec9/src/main.zig
@@ -2,11 +2,11 @@ const std = @import("std");
 
 pub fn main() !void {
     try test_process("test.txt", 6, 114);
-    const real_sum = try process("input.txt");
+    const real_sum = try process("input.txt", 21);
     std.debug.print("The answer for part 1 is {d}.\n", .{real_sum});
 }
 
-fn test_process(filename: []const u8, comptime n: comptime_int, answer: u64) !void {
+fn test_process(filename: []const u8, comptime n: comptime_int, answer: i64) !void {
     const test_sum = try process(filename, n);
     if (test_sum == answer) {
         std.debug.print("TEST PASSED\n", .{});
@@ -18,14 +18,15 @@ fn test_process(filename: []const u8, comptime n: comptime_int, answer: u64) !vo
 
 const Ident = [3]u8;
 
-fn process(filename: []const u8, comptime n: comptime_int) !u64 {
+fn process(filename: []const u8, comptime n: comptime_int) !i64 {
     const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
     var reader = file.reader();
     var ch: u8 = undefined;
-    var line = std.mem.zeroInit([n]i64, undefined);
+    var line = std.mem.zeroes([n]i64);
     var line_i: usize = 0;
-    var num: u64 = 0;
+    var num: i64 = 0;
+    var num_neg: bool = false;
     var sum: i64 = 0;
     while (true) {
         ch = reader.readByte() catch |err| switch (err) {
@@ -35,14 +36,22 @@ fn process(filename: []const u8, comptime n: comptime_int) !u64 {
         if (ch == '\n') {
             sum += process_line(line, n);
             num = 0;
+            num_neg = false;
             line_i = 0;
         } else if (ch == ' ') {
-            line[line_i] = num;
+            if (num_neg) {
+                line[line_i] = -num;
+            } else {
+                line[line_i] = num;
+            }
             line_i += 1;
             num = 0;
+            num_neg = false;
         } else if (ch >= '0' and ch <= '9') {
             num *= 10;
             num += ch - '0';
+        } else if (ch == '-') {
+            num_neg = true;
         }
     }
     return sum;
@@ -50,10 +59,10 @@ fn process(filename: []const u8, comptime n: comptime_int) !u64 {
 
 fn process_line(line_untyped: anytype, comptime n: comptime_int) i64 {
     const line = @as([n]i64, line_untyped);
-    var diff = std.mem.zeroInit([n]i64, undefined);
+    var diff = std.mem.zeroes([n]i64);
     for (0..n) |i| diff[i] = line[i];
-    var diff_len = n;
-    var lasts = std.mem.zeroInit([n]i64, undefined);
+    var diff_len: usize = @as(usize, n);
+    var lasts = std.mem.zeroes([n]i64);
     var lasts_i: usize = 0;
     while (!allZeroes(diff, n, diff_len)) {
         lasts[lasts_i] = diff[diff_len - 1];
@@ -76,6 +85,6 @@ fn extrapolate(lasts_untyped: anytype, lasts_len: usize, comptime n: comptime_in
 }
 
 fn allZeroes(arr: anytype, comptime n: comptime_int, n_use: usize) bool {
-    for (@as([n]i64, arr)) |i| if (i < n_use and arr[i] != 0) return false;
+    for (0..n_use) |i| if (@as([n]i64, arr)[i] != 0) return false;
     return true;
 }
